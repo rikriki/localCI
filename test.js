d41b931831126b361586183708fa59a67a9970c3
rikriki <ricky@dev13.datablocks.net>
2019-07-03 14:50:31 -0400

    add test

---
+++ test.js
@@ -0,0 +1,8 @@
+var should=require('chai').should();
+const chai = require('chai');
+
+describe('Foo test ', function () {
+ it('GET Foo: ', function (done) {
+    "Foo".should.equal("foo",'Foo should equal to Foo')
+ })
+})
